c0f784c046c6ae0fbdd6067329124e45640cda38
Wouter Mooij <wouter@wercker.com>
2013-10-24 15:51:53 +0200

    first b

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@
 #check if publish url starts with ftp
 #check if publish url endswith wwwroot
 
-FTP_USERNAME=$WERCKER_AZURE_FTP_DEPLOY_PUBLISH_URL
+FTP_URL=$WERCKER_AZURE_FTP_DEPLOY_PUBLISH_URL
 FTP_PASSWORD=$WERCKER_AZURE_FTP_DEPLOY_PASSWORD
-FTP_USER=$WERCKER_AZURE_FTP_DEPLOY_USERNAME
+FTP_USERNAME=$WERCKER_AZURE_FTP_DEPLOY_USERNAME
 
 echo "find . -type f -exec curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;"
 find . -type f -exec curl -u $FTP_USERNAME:$FTP_PASSWORD --ftp-create-dirs -T {} $FTP_URL/{} \;
